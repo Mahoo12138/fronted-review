6674a0d5709ac0fb1264442bddd28eca950ce829
mahoo12138 <mahoo12138@qq.com>
2026-04-01 00:05:12 +0800

    refactor(routes): 路由路径从 framework 调整为 frameworks
    重命名路由路径从 /framework/mobx 到 /frameworks/mobx，
    同时调整了文件目录结构以匹配新的路由约定。

--- src/routes/frameworks/mobx.tsx
+++ src/routes/frameworks/mobx.tsx
@@ -1,6 +1,6 @@
 import MobxTutorial from '@/pages/framework/mobx'
 import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/framework/mobx')({
+export const Route = createFileRoute('/frameworks/mobx')({
   component: MobxTutorial,
 })
