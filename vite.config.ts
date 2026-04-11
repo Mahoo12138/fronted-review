a1474b0215324c17a72609dbf99d036715a686b0
mahoo12138 <mahoo12138@qq.com>
2026-04-11 22:34:07 +0800

    feat(vite): 集成 MDX 支持
    添加 @mdx-js/rollup 依赖包并配置 Vite 插件以支持 MDX 文件处理，
    用于增强文档和组件的渲染能力。

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import path from 'path'
 import { defineConfig } from 'vite'
+import mdx from '@mdx-js/rollup'
 import react from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
@@ -8,6 +9,7 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    mdx(),
     react(),
     tanstackRouter(),
     vanillaExtractPlugin(),
