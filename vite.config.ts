da05a8af4f63721f2e774fb2555aefb0952470e7
mahoo12138 <mahoo12138@qq.com>
2026-04-20 22:48:12 +0800

    feat: 添加新依赖项并配置 mdx 插件
    - 在.gitignore中添加dist目录
    - 添加remark-callouts依赖用于处理注释块
    - 添加remark-gfm依赖用于GitHub Flavored Markdown支持

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import mdx from '@mdx-js/rollup'
-import react from '@vitejs/plugin-react'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-
+import path from "path";
+import { defineConfig } from "vite";
+import mdx from "@mdx-js/rollup";
+import remarkGfm from "remark-gfm";
+import remarkCallouts from "remark-callouts";
+import react from "@vitejs/plugin-react";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    mdx(),
+    mdx({
+      remarkPlugins: [remarkGfm, remarkCallouts], // 启用 callout 解析
+    }),
     react(),
     tanstackRouter(),
     vanillaExtractPlugin(),
@@ -19,4 +22,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
